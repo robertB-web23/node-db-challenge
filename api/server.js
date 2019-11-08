27d4f82b256a0b771feb3c6e8dbfbe8a8ad1abf6
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-08 12:47:49 -0500

    server setup

---
+++ api/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const helmet = require("helmet");
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+
+server.get('/', (req, res) => {
+    res.send("I am here")
+})
+
+module.exports = server;
