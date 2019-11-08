f17082c0d3c00cc8d6433e62009d5f6694209ecf
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-08 13:00:32 -0500

    tables and seeds

---
+++ data/seeds/003-tasks.js
@@ -0,0 +1,26 @@
+exports.seed = function(knex) {
+  return knex('tasks').insert([
+    {
+      project_id: 1, 
+      description: 'open can.', 
+      completed: true
+    },
+    {
+      project_id: 3, 
+      description: 'wash towels', 
+      completed: true
+    },
+    {
+      project_id: 2, 
+      description: 'sharpen pen', 
+      completed: true
+    },
+    {
+      project_id: 4, 
+      description: 'Walk Ben to the park and back', 
+      completed: true
+    }
+
+  ]);
+    
+};
